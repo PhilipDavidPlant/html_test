baa235294c91c3ac6e2f000254e648d0a5308c84
PhilipDavidPlant <philip.d.plant@gmail.com>
2017-03-11 13:44:59 +1300

    added skeleton for the first few views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { SettingsViewComponent } from './views/settings-view/settings-view.component';
+import { ManageListingsViewComponent } from './views/manage-listings-view/manage-listings-view.component';
+import { IndividualListingViewComponent } from './views/individual-listing-view/individual-listing-view.component';
+import { DirectoryViewComponent } from './views/directory-view/directory-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginViewComponent,
+    SettingsViewComponent,
+    ManageListingsViewComponent,
+    IndividualListingViewComponent,
+    DirectoryViewComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/individual-listing-view/individual-listing-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wb-individual-listing-view',
+  templateUrl: './individual-listing-view.component.html',
+  styleUrls: ['./individual-listing-view.component.css']
+})
+export class IndividualListingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
